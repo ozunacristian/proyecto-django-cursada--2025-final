4a665abd10f166e765613d462ca5abf6d01d0d2c
ozunacristian <ozunacristianj@gmail.com>
2025-12-08 12:00:51 -0300

    registra modelo tablero en admin.

--- apps/tablero/admin.py
+++ apps/tablero/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from apps.tablero.models import Tablero
 
-# Register your models here.
+
+@admin.register(Tablero)
+class TableroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'creado_por', 'fecha_creacion')
+    list_filter = ('creado_por', 'fecha_creacion')
+    search_fields = ('titulo', 'descripcion')
+    ordering = ('-fecha_creacion',)
