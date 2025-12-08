d21e6ccd6a0bd9d6fec8978bc6dbebb3012ab385
ozunacristian <ozunacristianj@gmail.com>
2025-12-08 11:55:31 -0300

    Actualiza modelo y admin de aplicacion usuario

--- apps/usuario/admin.py
+++ apps/usuario/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from apps.usuario.models import PerfilUsuario
 
-# Register your models here.
+@admin.register(PerfilUsuario)
+class PerfilUsuarioAdmin(admin.ModelAdmin):
+    list_display = ('usuario_username', 'rol')
+
+    # Esto permite mostrar el username del User relacionado
+    def usuario_username(self, obj):
+        return obj.user.username
+    
+    usuario_username.short_description = 'Usuario'
--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class PerfilUsuario(models.Model):
+    # Opciones para el rol
+    OPCIONES_ROLES = [
+        ('admin', 'Administrador'),
+        ('supervisor', 'Supervisor'),
+        ('empleado', 'Empleado'),
+        ('cliente', 'Cliente'),
+    ]
+
+    # Relación 1:1 con User
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='perfil')
+    
+    # Seleccion de rol
+    rol = models.CharField(max_length=20, choices=OPCIONES_ROLES)
+
+    def __str__(self):
+        return f"{self.user.username} ({self.get_rol_display()})"
