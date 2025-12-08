cbd933a95334bb4522175f6b3ed9ee301f102f15
ozunacristian <ozunacristianj@gmail.com>
2025-12-08 11:59:16 -0300

    crea modelo de aplicacion tablero.

--- apps/tablero/models.py
+++ apps/tablero/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tablero(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True, null=True)
+    creado_por = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tableros_creados')
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
