cb6c8b1294ff83415c7a95e94e7150091a1fe5df
ozunacristian <ozunacristianj@gmail.com>
2025-12-08 23:50:06 -0300

    comentario aclaratorio.

--- apps/ticket/models.py
+++ apps/ticket/models.py
@@ -4,7 +4,7 @@ from apps.tablero.models import Tablero
 
 class Lista(models.Model):
     titulo = models.CharField(max_length=200)
-    orden = models.PositiveIntegerField(default=1)
+    orden = models.PositiveIntegerField(default=1) # restrigimos a valores positivos iniciando en 1.
     tablero = models.ForeignKey(Tablero, on_delete=models.CASCADE, related_name='listas') # cada "columna" pertenece a un tablero.
     fecha_creacion = models.DateTimeField(auto_now_add=True)
 
@@ -12,7 +12,7 @@ class Lista(models.Model):
         ordering = ['orden'] # esto es para ordenar las "columnas"
         verbose_name = 'Lista'
         verbose_name_plural = 'Listas'
-        unique_together = ('tablero', 'orden') # para que no se pueda poner una lista en el mismo lugar de otra.
+        unique_together = ('tablero', 'orden') # para que no se pueda poner una lista en el mismo lugar de otra. (el id de tablero junto al valor de orden son únicos)
 
     def __str__(self):
         return f"{self.titulo} ({self.tablero.titulo})"
