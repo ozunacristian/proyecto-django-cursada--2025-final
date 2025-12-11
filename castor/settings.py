85861d34826943a023b8b3e598d58ca5829e616c
Steph <stephaniemelisav@gmail.com>
2025-12-11 18:36:30 -0300

    Template base.html con Bootstrap 5, configuración de templates en settings.py

--- castor/settings.py
+++ castor/settings.py
@@ -38,6 +38,7 @@ DJANGO_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_bootstrap5',
 ]
 
 LOCAL_APPS = [
@@ -135,5 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',  # ¡IMPORTANTE!
+]
 
 DEFAULT_CHARSET = 'utf-8'
