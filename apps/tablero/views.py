9ef4ae925bb5b5c036446d3760779b98565a0200
Steph <stephaniemelisav@gmail.com>
2025-12-11 18:51:37 -0300

    adaptación de vistas crud para el template

--- apps/tablero/views.py
+++ apps/tablero/views.py
@@ -1,31 +1,35 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from apps.tablero.models import Tablero
+from django.contrib.auth.models import User
 
 class TableroListView(ListView):
     model = Tablero
-    template_name = 'tablero/lista_tableros.html'
-    context_object_name = 'tableros'
+    template_name = 'tablero/lista.html'
 
 class TableroDetailView(DetailView):
     model = Tablero
-    template_name = 'tablero/detalle_tablero.html'
-    context_object_name = 'tablero'
+    template_name = 'tablero/detalle.html'
 
 class TableroCreateView(CreateView):
     model = Tablero
-    template_name = 'tablero/crear_tablero.html'
-    fields = ['titulo', 'descripcion', 'creado_por']
+    template_name = 'tablero/formulario.html'
+    fields = ['titulo', 'descripcion']  # IMPORTANTE: solo estos campos
     success_url = reverse_lazy('lista_tableros')
+    
+    def form_valid(self, form):
+        # Para pruebas, asigna el primer usuario que encuentre
+        user = User.objects.first()
+        form.instance.creado_por = user
+        return super().form_valid(form)
 
 class TableroUpdateView(UpdateView):
     model = Tablero
-    template_name = 'tablero/editar_tablero.html'
-    fields = ['titulo', 'descripcion', 'creado_por']  # De momento así, luego se automatiza el "creado_por"
+    template_name = 'tablero/formulario.html'
+    fields = ['titulo', 'descripcion']
     success_url = reverse_lazy('lista_tableros')
 
 class TableroDeleteView(DeleteView):
     model = Tablero
-    template_name = 'tablero/confirmar_eliminar_tablero.html'
+    template_name = 'tablero/eliminar.html'
     success_url = reverse_lazy('lista_tableros')
-
